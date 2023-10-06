d531045f1ff303f2af18d77bd481080027e783e4
Merouane Bellaha <bellaha.merouane@gmail.com>
2023-10-06 16:04:48 +0200

    [ADD] view + logic

---
+++ Frankenstein/Frankenstein/FrankensteinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrankensteinApp.swift
+//  Frankenstein
+//
+//  Created by Merouane Bellaha on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FrankensteinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
